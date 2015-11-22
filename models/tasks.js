c5122c5e0c31bf5278252c845eef9dffafcffc73
Blake Hagen <blakehagen@gmail.com>
2015-11-22 15:56:42 -0700

    added moment.js library

--- models/tasks.js
+++ models/tasks.js
@@ -1,10 +1,11 @@
+var moment = require('moment');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var taskSchema = new Schema({
-    name: String,
-    createdAt: {type: Date, default: Date.now},
-    due_in_hours: {type: Number}
+    name: { type: String },
+    createdAt: { type: String, default: moment().format('ddd, MMM D YYYY, h:mma') },
+    due_in_hours: { type: Number }
 });
 
 var Task = mongoose.model('Task', taskSchema);
